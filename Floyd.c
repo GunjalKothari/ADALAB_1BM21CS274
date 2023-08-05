50b6fd44e7396300f8c0ab44ad50ac4a2d26d945
GunjalKothari <125436922+GunjalKothari@users.noreply.github.com>
2023-08-05 12:27:13 +0530

    Create Floyd.c

---
+++ Floyd.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdbool.h>
+#define INF 999999
+void floydWarshall(int graph[][4], int V) {
+    int dist[V][V];
+    int i, j, k;
+
+    // Initialize the distance matrix with the given graph.
+    for (i = 0; i < V; i++) {
+        for (j = 0; j < V; j++) {
+            dist[i][j] = graph[i][j];
+        }
+    }
+
+    // Find all pairs shortest path by trying all possible intermediate vertices.
+    for (k = 0; k < V; k++) {
+        for (i = 0; i < V; i++) {
+            for (j = 0; j < V; j++) {
+                // If vertex k is on the shortest path from i to j, update the distance.
+                if (dist[i][k] + dist[k][j] < dist[i][j]) {
+                    dist[i][j] = dist[i][k] + dist[k][j];
+                }
+            }
+        }
+    }
+
+    // Print the shortest distance matrix.
+    printf("Shortest distance between every pair of vertices:\n");
+    for (i = 0; i < V; i++) {
+        for (j = 0; j < V; j++) {
+            if (dist[i][j] == INF) {
+                printf("%7s", "INF");
+            } else {
+                printf("%7d", dist[i][j]);
+            }
+        }
+        printf("\n");
+    }
+}
+
+int main() {
+    int V = 4; // Number of vertices in the graph
+    int graph[4][4] = {
+        {0, 5, INF, 10},
+        {INF, 0, 3, INF},
+        {INF, INF, 0, 1},
+        {INF, INF, INF, 0}
+    };
+
+    floydWarshall(graph, V);
+    return 0;
+}
+
